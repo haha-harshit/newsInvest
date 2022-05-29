1a4da61bd929482d2581ff6869457aeb1d5f208d
Harshit Kumar <bkharshitbk@gmail.com>
2022-05-30 00:35:12 +0530

    maintaining the 4x3 grid

--- src/Components/News.js
+++ src/Components/News.js
@@ -5,12 +5,20 @@ export class News extends Component {
   render() {
     return (
         <>
-        <NewsItem title="Shane Warn retired from cricket." description="On Monday evening announcement came that he'll not be able to play cricket from now as going through a serious medical condition."/>
-        <NewsItem title="Shane Warn retired from cricket." description="On Monday evening announcement came that he'll not be able to play cricket from now as going through a serious medical condition."/>
-        <NewsItem title="Shane Warn retired from cricket." description="On Monday evening announcement came that he'll not be able to play cricket from now as going through a serious medical condition."/>
-        <NewsItem title="Shane Warn retired from cricket." description="On Monday evening announcement came that he'll not be able to play cricket from now as going through a serious medical condition."/>
-        <NewsItem title="Shane Warn retired from cricket." description="On Monday evening announcement came that he'll not be able to play cricket from now as going through a serious medical condition."/>
-        <NewsItem title="Shane Warn retired from cricket." description="On Monday evening announcement came that he'll not be able to play cricket from now as going through a serious medical condition."/>
+        <div className="container my-5">
+          <h2 className="my-5">News Invest | Top Trending News</h2>
+          <div className="row">
+            <div className="col-md-4">
+              <NewsItem title="Shane Warn retired from cricket." description="On Monday evening announcement came that he'll not be able to play cricket from now as going through a serious medical condition."/>
+            </div>
+            <div className="col-md-4">
+              <NewsItem title="Shane Warn retired from cricket." description="On Monday evening announcement came that he'll not be able to play cricket from now as going through a serious medical condition."/>
+            </div>  
+            <div className="col-md-4">
+              <NewsItem title="Shane Warn retired from cricket." description="On Monday evening announcement came that he'll not be able to play cricket from now as going through a serious medical condition."/>
+            </div>
+          </div>
+        </div>
         </>
     )
   }
