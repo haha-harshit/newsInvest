28accbe20b8391b1dc8680c5628d9e062d6a1ac4
Harshit Kumar <bkharshitbk@gmail.com>
2022-05-30 00:46:06 +0530

    try card

--- src/Components/News.js
+++ src/Components/News.js
@@ -9,7 +9,7 @@ export class News extends Component {
           <h2 className="my-5">News Invest | Top Trending News</h2>
           <div className="row">
             <div className="col-md-4">
-              <NewsItem title="Shane Warn retired from cricket." description="On Monday evening announcement came that he'll not be able to play cricket from now as going through a serious medical condition."/>
+              <NewsItem title="Shane Warne memorial - watch & follow updates" description="Watch live coverage and follow text updates and tributes from the state memorial for Australian cricket legend Shane Warne at the Melbourne Cricket Ground." newsLink="http://www.bbc.co.uk/sport/live/cricket/60916236" newsImg="https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png"/>
             </div>
             <div className="col-md-4">
               <NewsItem title="Shane Warn retired from cricket." description="On Monday evening announcement came that he'll not be able to play cricket from now as going through a serious medical condition."/>
