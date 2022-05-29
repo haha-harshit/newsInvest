12a78a3eaf9a171228774d510f5aaaae4d8c29a7
Harshit Kumar <bkharshitbk@gmail.com>
2022-05-29 05:55:42 +0530

    create a News component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import { NavBar } from './Components/NavBar'
+import News from './Components/News'
 
 export class App extends Component {
   render() {
     return (
+      <>
       <NavBar/>
+      <News/>
+      </>
     )
   }
 }
---
+++ src/Components/News.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>This is a news</div>
+    )
+  }
+}
+
+export default News
